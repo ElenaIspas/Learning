e7e296b817e43392c2498be8a73629fa772fef0b
elena <elena.ispas@icloud.com>
2016-11-02 12:40:45 +0200

    First commit

---
+++ Learning/Learning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Learning
+//
+//  Created by Elena Ispas on 02/11/16.
+//  Copyright © 2016 ElenaI. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
